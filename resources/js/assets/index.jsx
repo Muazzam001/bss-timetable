bd9fb146d42eec88273bce8a889b2dbbe6b032ad
Muazzam Younus <muazzam.y@retailinsight.io>
2023-08-28 15:17:25 +0500

    Manage School Updates

--- resources/js/assets/index.jsx
+++ resources/js/assets/index.jsx
@@ -10,7 +10,14 @@ import plus from "./images/plus.png"
 import section from "./images/section.png"
 import ArrowDown from "./icons/arrowDown";
 import ArrowUp from "./icons/arrowUp";
-
+import whole from "./images/whole.png";
+import textBooks from "./images/text-books.png";
+import teacher from "./images/teacher.png";
+import settings from "./images/settings.png";
+import facilitator from "./images/facilitator.png";
+import classroom from "./images/classroom.png";
+import hamburger from "./images/hamburger.png";
+import close from "./images/close.png";
 
 export const assets = {
     signIn,
@@ -25,5 +32,12 @@ export const assets = {
     ArrowDown,
     ArrowUp,
     section,
+    whole,
+    textBooks,
+    teacher,
+    settings,
+    facilitator,
+    classroom,
+    hamburger,
+    close,
 }
-
--- resources/js/src/shared/input/inputField.jsx
+++ resources/js/src/shared/input/inputField.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Image from './image';
+import Image from '../image/image';
 
 const InputField = ({ icon, type, name, placeholder, className, value, onChange, error, rounded, htmlFor, label }) => {
     return (
@@ -11,14 +11,14 @@ const InputField = ({ icon, type, name, placeholder, className, value, onChange,
             ) : null}
             <div className="relative mt-1">
                 {icon ? (
-                    <div className="pointer-events-none absolute inset-y-2 rounded-full left-3 h-9 w-9 flex items-center justify-center p-[5px] bg-icon-bg">
-                        <Image src={icon} alt="input field icon" className='h-5 w-5' />
+                    <div className="pointer-events-none absolute inset-y-2 rounded-full left-3 h-9 w-9 flex items-center justify-center p-[5px] bg-blue-bg-icon">
+                        <Image src={icon} alt="Input field icon" className='h-5 w-5' />
                     </div>
                 ) : null}
                 <input
                     type={type}
                     name={name}
-                    className={`inline-block w-full ${rounded ? "rounded-full" : "rounded-lg"} font-primary placeholder:font-primary border-gray-300 hover:border-sky-blue hover:outline-0 hover:ring-0 focus:outline-0 focus:ring-0 bg-input-bg py-4 ${icon ? "pl-15" : "pl-5"} sm:text-sm ${className}`}
+                    className={`inline-block w-full ${rounded ? "rounded-full" : "rounded-lg"} font-primary placeholder:font-primary border-gray-300 hover:border-sky-blue hover:outline-0 hover:ring-0 focus:outline-0 focus:ring-0 bg-gray-bg-input py-4 ${icon ? "pl-15" : "pl-5"} sm:text-sm ${className}`}
                     placeholder={placeholder}
                     value={value}
                     onChange={onChange}
