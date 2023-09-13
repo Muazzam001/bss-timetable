6761e23f8134fce8dbf970b73ecf9026ca5545d5
Muazzam Younus <muazzam.y@retailinsight.io>
2023-09-14 02:17:08 +0500

    Whole Calender Update

--- resources/js/src/shared/inputSelect/inputSelect.jsx
+++ resources/js/src/shared/inputSelect/inputSelect.jsx
@@ -1,11 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import MenuItem from '@mui/material/MenuItem';
 import Select from '@mui/material/Select';
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import InputBase from '@mui/material/InputBase';
 import { styled } from '@mui/material/styles';
 
-const InputSelect = ({ options, className, defaultValue, setState, value, width }) => {
+const InputSelect = ({ options, className, defaultValue, setState, value, width, disabled, label }) => {
     const BootstrapInput = styled(InputBase)(({ theme }) => ({
 
         'label + &': {
@@ -16,20 +16,29 @@ const InputSelect = ({ options, className, defaultValue, setState, value, width 
             borderRadius: 6,
             position: 'relative',
             backgroundColor: "#F7F7F6",
-            border: '1px solid #1D5891',
+            border: `1px solid ${disabled && disabled ? "#D4D4D4" : "#1D5891"}`,
             fontSize: 16,
-            padding: '10px 26px 10px 12px',
+            padding: '0 26px 0 12px',
             '&:focus': {
                 borderRadius: 6,
             },
         },
+        '& .MuiPaper-root': {
+            zIndex: 999,
+        },
+        '& .MuiSelect-select.css-1uwzc1h-MuiSelect-select-MuiInputBase-input': {
+            display: "flex",
+            alignItems: "center",
+            height: "37px",
+            fontSize: "13px",
+        }
     }));
 
 
     const optionItems = options?.map((item, index) => {
         return (
-            <MenuItem key={`${index}`} sx={{width: `${width}`}} value={item}
-                      className='bg-lime-500 z-50'>{item}</MenuItem>
+            <MenuItem key={`${index}`} sx={{ width: `${width}` }} value={item}
+                className='z-50 whitespace-normal'>{item}</MenuItem>
         )
     });
 
@@ -39,49 +48,68 @@ const InputSelect = ({ options, className, defaultValue, setState, value, width 
 
     return (
         <>
-            <Select
-                value={value}
-                onChange={handleChange}
-                className={`bg-transparent border-0 ${className}`}
-                sx={{width: `${width}`}}
-                input={<BootstrapInput />}
-                defaultValue={defaultValue ? defaultValue : ""}
-                IconComponent={ExpandMoreIcon}
-                MenuProps={{
-                    PaperProps: {
-                        sx: {
-                            bgcolor: 'transparent',
-                            boxShadow: "none",
-                            paddingX: "10px",
-                            zIndex: 100000000,
-                            '& .MuiPaper-root': {
-                                left: "0px",
-                            },
-                            '& .MuiList-root': {
-                                padding: 0,
-                                width: { width },
-                            },
-                            '& .MuiMenuItem-root': {
-                                color: "#7D7D92",
-                                paddingX: 1,
-                                marginTop: "5px",
-                                marginBottom: "5px",
-                                bgcolor: "#F7F7F6",
-                                border: 1,
-                                borderColor: "#E8E8E8",
-                                borderRadius: "6px",
-                                boxShadow: "1px 1px 4px #babac4",
-                                '&:hover': {
-                                    bgcolor: "#175088",
-                                    color: "white"
-                                }
+            <div className="relative max-lg:flex-auto">
+                <Select
+                    value={value}
+                    onChange={handleChange}
+                    className={`bg-transparent z-50 border-0 h-10 text-xs ${className}`}
+                    sx={{ width: `${width}` }}
+                    input={<BootstrapInput />}
+                    defaultValue={defaultValue ? defaultValue : ""}
+                    IconComponent={ExpandMoreIcon}
+                    disabled={disabled && disabled}
+                    MenuProps={{
+                        PaperProps: {
+                            sx: {
+                                bgcolor: 'transparent',
+                                boxShadow: "none",
+                                paddingX: "10px",
+                                zIndex: 100000000,
+                                '& .MuiPaper-root': {
+                                    left: "0px",
+                                },
+                                '& .MuiList-root': {
+                                    padding: 0,
+                                    minWidth: "fit-content",
+                                    width: { width },
+                                },
+                                '& .MuiButtonBase-root': {
+                                    whiteSpace: "normal",
+                                },
+                                '& .MuiMenuItem-root.Mui-selected': {
+                                    bgcolor: "#CEE8F5",
+                                },
+                                '& .MuiMenuItem-root': {
+                                    fontSize: "13px",
+                                    whiteSpace: 'whitespace-normal',
+                                    color: "#7D7D92",
+                                    paddingX: 1,
+                                    marginTop: "5px",
+                                    marginBottom: "5px",
+                                    bgcolor: "#F7F7F6",
+                                    border: 1,
+                                    borderColor: "#E8E8E8",
+                                    borderRadius: "6px",
+                                    boxShadow: "1px 1px 4px #babac4",
+                                    '&:hover': {
+                                        bgcolor: "#175088",
+                                        color: "white"
+                                    }
+                                },
+                                '& .MuiMenuItem-root:first-of-type': {
+                                    bgcolor: "#F7F7F6",
+                                    '&:hover': {
+                                        bgcolor: "#175088",
+                                        color: "white"
+                                    }
+                                },
                             },
                         },
-                    },
-                }}
-            >
-                {optionItems}
-            </Select>
+                    }}
+                >
+                    {optionItems}
+                </Select>
+            </div>
         </>
     )
 }
